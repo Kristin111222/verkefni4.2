40a09518598cd3362a22849275ca3c653f391a56
kristin900 <kristineyjar@gmail.com>
2025-02-15 16:02:24 +0000

    liður 1

---
+++ ass4/src/main/java/is/hi/ksr11/hbv202g/ass4/App.java
@@ -0,0 +1,13 @@
+package is.hi.ksr11.hbv202g.ass4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
